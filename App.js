8d106dfe32322176efc75e23fe866030f4597768
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-06-16 10:30:27 +0900

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}>
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 }
@@ -13,7 +16,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  yellowView: {
+    flex: 2,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex:1,
+    backgroundColor: "blue"
+  }
 });
